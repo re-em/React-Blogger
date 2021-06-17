7223cb4d653ad629befd6397c6de1deb75f505ff
SohaMohamed98 <48870254+SohaMohamed98@users.noreply.github.com>
2021-06-17 23:37:02 +0200

    React-Blogger

---
+++ client/src/components/dashboard/DashboardActions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DashboardActions = () => {
+  return (
+    <div className='dash-buttons'>
+      
+    </div>
+  );
+};
+
+export default DashboardActions;
